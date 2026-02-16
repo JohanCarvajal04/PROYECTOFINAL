bd5555f674d51bab5652dcd264ca83c1bb3c5819
011011bjf <bfigueroam@uteq.edu.ec>
2026-02-16 00:31:46 -0500

    Inicio_notificaciones

--- Backend/src/main/java/com/app/uteq/BackendApplication.java
+++ Backend/src/main/java/com/app/uteq/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.app.uteq;
 
-import com.app.uteq.Config.RsaKeyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,10 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.app.uteq.Config.RsaKeyConfig;
+
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyConfig.class)
 public class BackendApplication {
 
+
+    
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
